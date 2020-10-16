fefdae44c60b0a003ad31c9088a685474e8a9a46
Stepan Uhryn <ugrinstepan@gmail.com>
2020-10-16 15:00:56 +0300

    home_work_9

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-let a = 1;
-const b = '2'
-const c = '9ло'
+let ate = prompt('Введіть першу дату у форматі рррр, мм, дд');
+let ate1 = prompt('Введіть іншу дату у форматі рррр, мм, дд');
+let date = new Date(ate);
+let date1 = new Date(ate1);
+
+// порівнюємо роки у цих двох датах
+
+if (date.getFullYear() < date1.getFullYear()) {
+        console.log(`Дата ${date.getFullYear()} є меншою ніж дата ${date1.getFullYear()}`);
+} else {
+    if (date.getFullYear() > date1.getFullYear()) {
+        console.log(`Дата ${date.getFullYear()} є більшою ніж дата ${date1.getFullYear()}`);
+           } 
+// роки однакові, тепер порівняємо місяці
+        else {
+          if (date.getMonth() < date1.getMonth()) {
+            console.log(`Дата ${date.getFullYear()}.${date.getMonth()} менша ніж дата ${date1.getFullYear()}.${date1.getMonth()}`);
+           } else {
+                if (date.getMonth() > date1.getMonth()) {
+                    console.log(`Дата ${date.getFullYear()}.${date.getMonth()} є більшою ніж дата ${date1.getFullYear()}.${date1.getMonth()}`)
+                } else {
+//  місяці однакові, значить потрібно порівняти дні у місяцях
+                      if (date.getDate() < date1.getDate()) {
+                           console.log(`Дата ${date.getFullYear()}.${date.getMonth()}.${date.getDate()} є меншою ніж дата ${date1.getFullYear()}.${date1.getMonth()}.${date1.getDate()}`)
+                        }  else {
+                                if (date.getDate() > date1.getDate()) {
+                                    console.log (`Дата ${date.getFullYear()}.${date.getMonth()}.${date.getDate()} більша ніж дата ${date1.getFullYear()}.${date1.getMonth()}.${date1.getDate()}`)
+                                    } else {
+// дні однакові, значить однакові і дати в цілому
+                                          console.log(`Дата ${date.getFullYear()}.${date.getMonth()}.${date.getDate()} однакова з датою ${date1.getFullYear()}.${date1.getMonth()}.${date1.getDate()}.`)
+                                            }
+                                }
+                    }
+                }
+            }
+ }
--- index1.js
+++ index1.js
@@ -1,5 +1,21 @@
-    let x = 5, y = 10, sum = x + y;
-    // Prints variable value
-        document.write(x + "<br>");
-        document.write(y + "<br>");
-        document.write(sum); 
+console.log(document.body);
+console.log(document.getElementById('id1'));
+console.log(document.getElementsByClassName('ddd'));
+console.log(document.getElementsByTagName('script'));
+
+const buttonClick = () => {
+    alert('We press button');
+}
+
+const button = document.getElementById('myButton')
+    if (button) {
+        button.onclick = buttonClick
+    }
+
+    const funcChange = (e) => {
+      console.log(e.target.value);
+    }
+    const input = document.getElementById('id3')
+    if (input) {
+        input.onchange = funcChange
+    }
